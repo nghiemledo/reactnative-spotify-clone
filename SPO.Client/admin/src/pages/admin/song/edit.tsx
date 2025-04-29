7953bb61d667eac070520fb094eb55df46e3e697
*gWRU5u1c7q <nghiemledo@gmail.com>
2025-04-29 12:19:46 +0700

    Feat: add ManageSongCreatePage, ManageSongEditPage, and ManageSongPage components for song management

---
+++ SPO.Client/admin/src/pages/admin/song/edit.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageSongEditPage: React.FC = () => {
+    return (
+        <div>
+            manage song edit page
+        </div>
+    );
+};
+
+export default ManageSongEditPage;
---
+++ SPO.Client/admin/src/pages/admin/song/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageSongPage: React.FC = () => {
+    return (
+        <div>
+            manage song page
+        </div>
+    );
+};
+
+export default ManageSongPage;
