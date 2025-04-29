3c6a53929c0bf39f046ec072af3921560182e7de
*gWRU5u1c7q <nghiemledo@gmail.com>
2025-04-29 17:37:44 +0700

    Feat: replace anchor tags with Link component in NavMain and simplify TeamSwitcher layout

--- SPO.Client/admin/src/components/team-switcher.tsx
+++ SPO.Client/admin/src/components/team-switcher.tsx
@@ -1,31 +1,17 @@
 import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import {
   SidebarMenu,
-  SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
-import AppLogo from '@/assets/images/logo.png'
+import AppLogo from '@/assets/images/spotify-logo.svg'
 
 export function TeamSwitcher() {
 
   return (
     <SidebarMenu>
       <SidebarMenuItem>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <SidebarMenuButton
-              size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-            >
-              <div className="h-20 w-full">
-                <img src={AppLogo} alt="Spotify Logo" className="w-full h-full object-contain" />
-              </div>
-            </SidebarMenuButton>
-          </DropdownMenuTrigger>
-        </DropdownMenu>
+        <div className="h-13 w-full">
+          <img src={AppLogo} alt="Spotify Logo" className="w-full h-full object-contain" />
+        </div>
       </SidebarMenuItem>
     </SidebarMenu>
   )
