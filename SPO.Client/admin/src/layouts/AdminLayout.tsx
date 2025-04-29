5b4282e01cb1725d8cf7cf6cd5c0672877669781
*gWRU5u1c7q <nghiemledo@gmail.com>
2025-04-29 13:33:37 +0700

    Feat: add theme toggle functionality with ModeToggle component and integrate it into AdminLayout

--- SPO.Client/admin/src/layouts/AdminLayout.tsx
+++ SPO.Client/admin/src/layouts/AdminLayout.tsx
@@ -1,10 +1,45 @@
+import { AppSidebar } from '@/components/app-sidebar';
+import { ModeToggle } from '@/components/mode-toggle';
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '@/components/ui/breadcrumb';
+import { Separator } from '@/components/ui/separator';
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const AdminLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
         <div>
-            {children}
-        </div>
+            <SidebarProvider>
+                <AppSidebar />
+                <SidebarInset>
+                    <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+                        <div className="flex justify-between w-full px-4">
+                            <div className="flex items-center gap-2">
+                                <SidebarTrigger className="-ml-1" />
+                                <Separator orientation="vertical" className="mr-2 h-4" />
+                                <Breadcrumb>
+                                    <BreadcrumbList>
+                                        <BreadcrumbItem className="hidden md:block">
+                                            <BreadcrumbLink asChild>
+                                                <Link to={'/admin'}>
+                                                    Dashboard
+                                                </Link>
+                                            </BreadcrumbLink>
+                                        </BreadcrumbItem>
+                                        <BreadcrumbSeparator className="hidden md:block" />
+                                        <BreadcrumbItem>
+                                            <BreadcrumbPage>Data fetching</BreadcrumbPage>
+                                        </BreadcrumbItem>
+                                    </BreadcrumbList>
+                                </Breadcrumb>
+                            </div>
+                            <ModeToggle />
+                        </div>
+                    </header>
+                    <div className='p-4'>{children}</div>
+                </SidebarInset>
+            </SidebarProvider>
+        </div> 
     );
 };
 
