a8f6798f48c90de8dd5fa0cb3d229b8abb449cc7
*gWRU5u1c7q <nghiemledo@gmail.com>
2025-04-29 12:19:57 +0700

    Feat: add ManageArtistCreatePage, ManageArtistEditPage, and ManageArtistPage components for artist management

---
+++ SPO.Client/admin/src/pages/admin/artist/create.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageArtistCreatePage: React.FC = () => {
+    return (
+        <div>
+            manage artist create page
+        </div>
+    );
+};
+
+export default ManageArtistCreatePage;
---
+++ SPO.Client/admin/src/pages/admin/artist/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageArtistPage: React.FC = () => {
+    return (
+        <div>
+            manage artist page
+        </div>
+    );
+};
+
+export default ManageArtistPage;
