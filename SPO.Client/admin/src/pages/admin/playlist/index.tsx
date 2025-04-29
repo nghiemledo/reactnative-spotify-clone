03f6ac7ac871daf144afa11f68851ae1efc0612c
*gWRU5u1c7q <nghiemledo@gmail.com>
2025-04-29 18:34:44 +0700

    Feat: add ManagePlaylist and ManageReportsPlaylist components for playlist management

---
+++ SPO.Client/admin/src/pages/admin/playlist/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManagePlaylist: React.FC = () => {
+    return (
+        <div>
+            manage playlist page
+        </div>
+    );
+};
+
+export default ManagePlaylist;
---
+++ SPO.Client/admin/src/pages/admin/playlist/reports.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageReportsPlaylist: React.FC = () => {
+    return (
+        <div>
+            manage playlist reports page
+        </div>
+    );
+};
+
+export default ManageReportsPlaylist;
