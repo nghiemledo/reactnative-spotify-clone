eedf8982f1a97e22bd0b73dc26de7bff7c9ea7a5
*gWRU5u1c7q <nghiemledo@gmail.com>
2025-05-01 20:29:26 +0700

    Feat: add UserRoles entities including Role, RoleClaim, User, UserClaim, UserLogin, UserRole, and UserToken

---
+++ SPO.Server/SPO.Domain/Entities/UserRoles/UserLogin.cs
@@ -0,0 +1,12 @@
+﻿using SPO.Domain.Entities.Base;
+
+namespace SPO.Domain.Entities.UserRoles
+{
+    public class UserLogin : AuditableEntity<string>
+    {
+        public string LoginProvider { get; set; } = null!;
+        public string ProviderKey { get; set; } = null!;
+        public string? ProviderDisplayName { get; set; }
+        public string? UserId { get; set; }
+    }
+}
