243e047534fcfafafb6baa53089ed13ac6afd4e3
*gWRU5u1c7q <nghiemledo@gmail.com>
2025-05-02 03:35:10 +0700

    Fix(server): update Song class to inherit from AuditableEntity<string> and clean up unused using directives

--- SPO.Server/SPO.Domain/Entities/Song.cs
+++ SPO.Server/SPO.Domain/Entities/Song.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SPO.Domain.Entities.Base;
+﻿using SPO.Domain.Entities.Base;
 
 namespace SPO.Domain.Entities;
 
-public class Song : AuditableEntity
+public class Song : AuditableEntity<string>
 {
+    public string? Title { get; set; }
+    public string? Slug { get; set; }
+    public string? Duration { get; set; }
+    public string? ReleaseDate { get; set; }
+    public string? Genre { get; set; }
+    public string? ArtistId { get; set; }
+    public string? AlbumId { get; set; }
 }
