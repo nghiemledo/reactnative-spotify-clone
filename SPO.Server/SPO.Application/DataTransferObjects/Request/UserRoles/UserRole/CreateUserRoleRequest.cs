1d71b0be43e1557bdf4251d4b40fd73b969a168b
*gWRU5u1c7q <nghiemledo@gmail.com>
2025-05-01 20:28:13 +0700

    Feat: add CreateUserRoleRequest and UpdateUserRoleRequest DTOs

---
+++ SPO.Server/SPO.Application/DataTransferObjects/Request/UserRoles/UserRole/CreateUserRoleRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace SPO.Application.DataTransferObjects.Request.UserRoles.UserRole
+{
+    public class CreateUserRoleRequest
+    {
+        public required string Id { get; set; }
+        public string UserId { get; set; } = null!;
+        public string RoleId { get; set; } = null!;
+        public bool? IsActive { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+}
