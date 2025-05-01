d3fe318fe1cad4743708ec26bbe6cc45cc4533b3
*gWRU5u1c7q <nghiemledo@gmail.com>
2025-05-01 14:30:21 +0700

    Feat: add axios client configuration with token management and refresh logic

---
+++ SPO.Client/admin/src/configs/api.config.ts
@@ -0,0 +1,87 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// export const PRODUCTION_API = process.env.NEXT_PUBLIC_API_URL
+export const PRIVATE_API = "https://localhost:44273/api"
+
+import axios from 'axios'
+
+const axiosClient = axios.create({
+    baseURL: PRIVATE_API,
+    headers: {
+        'content-type': 'application/json'
+    }
+})
+
+const getToken = () => localStorage.getItem("token") || "";
+const getRefreshToken = () => localStorage.getItem("refreshToken") || "";
+
+axiosClient.interceptors.request.use(async (config) => {
+    const token = getToken();
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+})
+
+let isRefreshing = false;
+let failedQueue: { resolve: (token: string) => void; reject: (error: any) => void }[] = [];
+
+const processQueue = (token: string | null, error: any = null) => {
+    failedQueue.forEach((prom) => {
+        if (token) {
+            prom.resolve(token);
+        } else {
+            prom.reject(error);
+        }
+    });
+    failedQueue = [];
+};
+
+const refreshToken = async () => {
+    try {
+        const response = await axios.post(`${PRIVATE_API}/refresh`, { refreshToken: getRefreshToken() });
+        const newAccessToken = response.data.token;
+
+        localStorage.setItem("token", newAccessToken);
+        return newAccessToken;
+    } catch (error) {
+        console.error("Failed to get refresh token:", error);
+        throw error;
+    }
+};
+
+axiosClient.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+
+        if (error.response?.status === 403) {
+            if (isRefreshing) {
+                return new Promise((resolve, reject) => {
+                    failedQueue.push({ resolve, reject });
+                }).then((token) => {
+                    originalRequest.headers.Authorization = `Bearer ${token}`;
+                    return axiosClient(originalRequest);
+                });
+            }
+
+            originalRequest._retry = true;
+            isRefreshing = true;
+
+            try {
+                const newAccessToken = await refreshToken();
+                processQueue(newAccessToken);
+                originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+                return axiosClient(originalRequest);
+            } catch (err) {
+                processQueue(null, err);
+                return Promise.reject(err);
+            } finally {
+                isRefreshing = false;
+            }
+        }
+
+        return Promise.reject(error);
+    }
+)
+
+export default axiosClient
