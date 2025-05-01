56db2c4a0bc7e29acbc6a2f7fcc4b43c2641a28c
*gWRU5u1c7q <nghiemledo@gmail.com>
2025-05-02 03:36:54 +0700

    Feat(server): implement ServiceCollectionExtensions for repository registration

--- SPO.Server/SPO.Infrastructure/EntityFramework/DbContexts/ApplicationDbContext.cs
+++ SPO.Server/SPO.Infrastructure/EntityFramework/DbContexts/ApplicationDbContext.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using SPO.Domain.Entities;
+using SPO.Domain.Entities.UserRoles;
 
 namespace SPO.Infrastructure.EntityFramework.DbContexts
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<RoleClaim> RoleClaims { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserClaim> UserClaims { get; set; }
+        public DbSet<UserLogin> UserLogins { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<UserToken> UserTokens { get; set; }
+        public DbSet<Song> Songs { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            //builder.Seed();
+        }
     }
 }
---
+++ SPO.Server/SPO.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SPO.Infrastructure.Dappers.Base;
+using SPO.Infrastructure.Repositories.UserRoles;
+
+namespace SPO.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddRepositories(this IServiceCollection services)
+        {
+            // Register custom repositories
+            AddRepository(services);
+        }
+        public static void AddRepository(this IServiceCollection services)
+        {
+            services.AddScoped<IDapperBase, DapperBase>();
+            services.AddScoped<IRoleRepository, RoleRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserRoleRepository, UserRoleRepository>();
+            //services.AddScoped<IFunctionRepository, FunctionRepository>();
+        }
+    }
+}
