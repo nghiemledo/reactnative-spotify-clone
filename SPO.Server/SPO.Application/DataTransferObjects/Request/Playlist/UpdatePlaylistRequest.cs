70cfc4e237134d3c05aaa5808efb576773b3bc3f
*gWRU5u1c7q <nghiemledo@gmail.com>
2025-05-05 10:51:29 +0700

    Feat(server): implement Playlist management (Create, Update, Delete, GetAll, GetById)

---
+++ SPO.Server/SPO.Application/DataTransferObjects/Request/Playlist/UpdatePlaylistRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace SPO.Application.DataTransferObjects.Request.Playlist
+{
+    public class UpdatePlaylistRequest
+    {
+        public string? Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string? CoverImage { get; set; }
+        public bool IsPublic { get; set; }
+        public string? UserId { get; set; }
+    }
+}
