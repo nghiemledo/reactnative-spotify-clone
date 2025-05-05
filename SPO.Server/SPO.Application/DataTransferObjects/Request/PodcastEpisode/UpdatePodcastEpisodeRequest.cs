d158fa43e0fec9504e3800aaaeb4c424ca56ceb5
*gWRU5u1c7q <nghiemledo@gmail.com>
2025-05-05 10:53:49 +0700

    Feat(server): implement PodcastEpisode management (Create, Update, Delete, GetAll, GetById)

---
+++ SPO.Server/SPO.Application/DataTransferObjects/Request/PodcastEpisode/UpdatePodcastEpisodeRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace SPO.Application.DataTransferObjects.Request.PodcastEpisode
+{
+    public class UpdatePodcastEpisodeRequest
+    {
+        public string? Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public int Duration { get; set; }
+        public string AudioUrl { get; set; } = string.Empty;
+        public DateTime? ReleaseDate { get; set; }
+        public string? ShowId { get; set; }
+    }
+}
