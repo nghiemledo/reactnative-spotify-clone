977fde3b489fb7f2396a34d6b43f942a4774135c
*gWRU5u1c7q <nghiemledo@gmail.com>
2025-05-05 10:54:20 +0700

    Feat(server): update GeneralProfile mappings

--- SPO.Server/SPO.Application/Mappings/GeneralProfile.cs
+++ SPO.Server/SPO.Application/Mappings/GeneralProfile.cs
@@ -1,7 +1,16 @@
 ﻿using AutoMapper;
+using SPO.Application.DataTransferObjects.Request.Album;
+using SPO.Application.DataTransferObjects.Request.Artist;
+using SPO.Application.DataTransferObjects.Request.Genre;
+using SPO.Application.DataTransferObjects.Request.Playlist;
+using SPO.Application.DataTransferObjects.Request.PodcastCategory;
+using SPO.Application.DataTransferObjects.Request.PodcastEpisode;
+using SPO.Application.DataTransferObjects.Request.PodcastShow;
+using SPO.Application.DataTransferObjects.Request.Song;
 using SPO.Application.DataTransferObjects.Request.UserRoles.Role;
 using SPO.Application.DataTransferObjects.Request.UserRoles.User;
 using SPO.Application.DataTransferObjects.Request.UserRoles.UserRole;
+using SPO.Domain.Entities;
 using SPO.Domain.Entities.UserRoles;
 
 namespace SPO.Application.Mappings
@@ -13,6 +22,16 @@ namespace SPO.Application.Mappings
             CreateMap<CreateRoleRequest, Role>();
             CreateMap<CreateUserRequest, User>();
             CreateMap<CreateUserRoleRequest, UserRole>();
+            CreateMap<CreateUserRoleRequest, UserToken>();
+            CreateMap<CreateGenreRequest, Genre>();
+            CreateMap<CreateArtistRequest, Artist>();
+            CreateMap<CreateSongRequest, Song>();
+            CreateMap<CreateAlbumRequest, Album>();
+            CreateMap<CreatePlaylistRequest, Playlist>();
+            CreateMap<CreatePodcastCategoryRequest, PodcastCategory>();
+            CreateMap<CreatePodcastShowRequest, PodcastShow>();
+            CreateMap<CreatePodcastEpisodeRequest, PodcastEpisode>();
+
         }
     }
 }
