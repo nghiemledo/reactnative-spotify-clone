ab05190a9408c719fb224ca8c4cc5a40e10dd520
*gWRU5u1c7q <nghiemledo@gmail.com>
2025-05-05 07:56:19 +0700

    Feat(server): implement CRUD stored procedures for Role management (GetRoleById, GetRoleByName, GetAllRoles, GetRoleNotInAdmin, InsertRole, UpdateRole, DeleteRole)

---
+++ SPO.Server/SPO.Infrastructure/Dappers/StoreProcedures/UserRoles/Role/SP_SPO_GetRoleById.sql
@@ -0,0 +1,20 @@
+USE [SPO_DB]
+GO
+
+/****** Object:  StoredProcedure [dbo].[SP_SPO_GetDomainById]    Script Date: 2/15/2025 7:50:17 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- drop procedure [SP_SPO_GetRoleById]
+create procedure [dbo].[SP_SPO_GetRoleById]
+    @Id nvarchar(255)
+as
+begin
+    select * from Roles where Id = @Id;
+end
+go
+
+-- exec [SP_SPO_GetRoleById] @Id = ''
