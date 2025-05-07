dc377f5c46d0a81926b809476860f6ed8b24ceb2
nxt <nxtnguyenxuantruongxt@gmail.com>
2025-05-07 16:07:44 +0700

    Feat(app): wrap App component with Redux Provider and PersistGate for state management

--- SPO.Client/app/App.tsx
+++ SPO.Client/app/App.tsx
@@ -3,14 +3,21 @@ import { config } from "./tamagui.config";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Text } from "react-native";
 import Navigation from "navigations";
+import { Provider } from "react-redux";
+import { persistor, store } from "store";
+import { PersistGate } from "redux-persist/integration/react";
 export default function App() {
   // console.log(tamaguiConfig);
   return (
-    <TamaguiProvider config={config} defaultTheme="light">
-      <SafeAreaProvider>
-        <Navigation />
-        {/* <Text>Welcome to Tamagui</Text> */}
-      </SafeAreaProvider>
-    </TamaguiProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <TamaguiProvider config={config} defaultTheme="light">
+          <SafeAreaProvider>
+            <Navigation />
+            {/* <Text>Welcome to Tamagui</Text> */}
+          </SafeAreaProvider>
+        </TamaguiProvider>
+      </PersistGate>
+    </Provider>
   );
 }
