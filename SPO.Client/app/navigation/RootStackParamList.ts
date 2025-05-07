1d64e04e7b51a56fe82dcd6f663bd8a99ef8a302
nxt <nxtnguyenxuantruongxt@gmail.com>
2025-05-07 13:42:54 +0700

    Refactor code structure for improved readability and maintainability

--- SPO.Client/app/navigation/RootStackParamList.ts
+++
@@ -1,6 +0,0 @@
-export type RootStackParamList = {
-  Home: undefined; // Tab Navigator (Home, Search, Library)
-  Album: { id: string }; // Album screen with an 'id' parameter
-  PlayerModal: undefined; // Player modal
-  PlayingScreen: undefined; // Playing screen
-};
