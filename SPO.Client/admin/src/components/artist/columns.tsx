ffa8ccc0594355b342a2f2b347f590735dbe1ca7
*gWRU5u1c7q <nghiemledo@gmail.com>
2025-05-07 15:58:57 +0700

    Feat(admin): implement artist management pages including add, edit, view, and list functionalities

---
+++ SPO.Client/admin/src/components/artist/columns.tsx
@@ -0,0 +1,137 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react-hooks/rules-of-hooks */
+import { ColumnDef } from "@tanstack/react-table";
+import { useState } from "react";
+import { toast } from "sonner";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip";
+import { EyeIcon } from "../eye-icon";
+import { EditIcon } from "../edit-icon";
+import { DeleteIcon } from "../delete-icon";
+import DeleteModal from "../delete-modal";
+import { useAppDispatch } from "@/store/store";
+import { useNavigate } from "react-router-dom";
+import { formatDate } from "@/utils/formatDate";
+import { deleteArtistData } from "@/store/artist/artist.actions";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Artist } from "@/types/artist.type";
+
+export const columns: ColumnDef<Artist>[] = [
+    {
+        accessorKey: "index",
+        header: "No.",
+        cell: ({ row }) => row.index + 1,
+    },
+    {
+        accessorKey: "name",
+        header: "Name",
+    },
+    {
+        accessorKey: "urlAvatar",
+        header: "Avatar",
+        cell: ({ row }) => {
+            return (
+                <Avatar>
+                    <AvatarImage className="object-cover w-full h-full" src={row.original.urlAvatar} />
+                    <AvatarFallback>AVATAR</AvatarFallback>
+                </Avatar>
+            )
+        }
+    },
+    {
+        accessorKey: "bio",
+        header: "Bio",
+    },
+    {
+        accessorKey: "createdAt",
+        header: "Created At",
+        cell: ({ row }) => {
+            return formatDate(row.original.createdAt!);
+        },
+    },
+    {
+        id: "actions",
+        header: "Actions",
+        cell: ({ row }) => {
+            const dispatch = useAppDispatch();
+            const navigate = useNavigate();
+            const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+            const [loading, setLoading] = useState(false);
+
+            const handleView = () => {
+                navigate(`/admin/artists/${row.original.id}`);
+            };
+
+            const handleEdit = () => {
+                navigate(`/admin/artists/${row.original.id}/edit`);
+            }
+
+            const handleDelete = async () => {
+                setLoading(true);
+                try {
+                    await dispatch(deleteArtistData(row.original.id!)).unwrap();
+                    toast('Success', {
+                        description: `Deleted data ${row.original.name}`,
+                    });
+                    setDeleteModalOpen(false);
+                } catch (error: any) {
+                    console.error('Delete failed:', error);
+                    toast('Error', {
+                        description: 'Can not delete data. Please try again later.',
+                    });
+                } finally {
+                    setLoading(false);
+                }
+            };
+
+            return (
+                <div className="relative flex items-center gap-3">
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <span className="text-lg cursor-pointer active:opacity-50"
+                                    onClick={handleView}
+                                >
+                                    <EyeIcon />
+                                </span>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                <p>View</p>
+                            </TooltipContent>
+                        </Tooltip>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <span className="text-lg cursor-pointer active:opacity-50"
+                                    onClick={handleEdit}
+                                >
+                                    <EditIcon />
+                                </span>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                <p>Update</p>
+                            </TooltipContent>
+                        </Tooltip>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <span className="text-lg text-red-400 cursor-pointer active:opacity-50"
+                                    onClick={() => setDeleteModalOpen(true)}
+                                >
+                                    <DeleteIcon />
+                                </span>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                <p>Delete</p>
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                    <DeleteModal
+                        deleteModalOpen={deleteModalOpen}
+                        setDeleteModalOpen={setDeleteModalOpen}
+                        row={row}
+                        loading={loading}
+                        handleDelete={handleDelete}
+                    />
+                </div>
+            )
+        }
+    },
+];
