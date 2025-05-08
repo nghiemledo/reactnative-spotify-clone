56b79c5174bac113df8de750572513f7c30b5770
DESKTOP-4ONK187\ACER NITRO 5 <hoanglong200703@gmail.com>
2025-05-09 02:30:29 +0700

    feat: update navigation and types for authentication screens and detail playlist

--- SPO.Client/app/navigation/index.tsx
+++ SPO.Client/app/navigation/index.tsx
@@ -1,11 +1,17 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from '../screens/HomeScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "../screens/HomeScreen";
 // import SearchScreen from '../screens/SearchScreen';
 // import LibraryScreen from '../screens/LibraryScreen';
 // import AlbumScreen from '../screens/AlbumScreen';
-import PlayerModal from '../components/PlayerModal';
+import PlayerModal from "../components/PlayerModal";
+import DetailPlaylistScreen from "../screens/DetailPlaylistScreen";
+import AlbumScreen from "../screens/AlbumScreen";
+import RegisterScreen from "../screens/RegisterScreen";
+import EmailRegisterScreen from "../screens/EmailRegisterScreen";
+import LoginScreen from "../screens/LoginScreen";
+import EmailLoginScreen from "../screens/EmailLoginScreen";
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -13,7 +19,30 @@ const Stack = createStackNavigator();
 function TabNavigator() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ headerShown: false }} // Thêm dòng này để tắt header
+      />
+      {/* <Tab.Screen name="DetailPlaylist" component={DetailPlaylistScreen} />
+      <Tab.Screen name="Album" component={AlbumScreen} /> */}
+      <Tab.Screen
+        name="detailplaylist"
+        component={DetailPlaylistScreen}
+        options={{ headerShown: false }}
+      />
+
+      <Tab.Screen
+        name="register"
+        component={RegisterScreen}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="login"
+        component={LoginScreen}
+        options={{ headerShown: false }}
+      />
+
       {/* <Tab.Screen name="Search" component={SearchScreen} />
       <Tab.Screen name="Library" component={LibraryScreen} /> */}
     </Tab.Navigator>
@@ -29,8 +58,22 @@ export default function Navigation() {
           component={TabNavigator}
           options={{ headerShown: false }}
         />
-        {/* <Stack.Screen name="Album" component={AlbumScreen} />
         <Stack.Screen
+          name="EmailLogin"
+          component={EmailLoginScreen}
+          options={{ headerShown: false }}
+        />
+        {/* <Stack.Screen
+          name="Player"
+          component={PlayerModal}
+          options={{ presentation: 'modal' }}
+        /> */}
+        <Stack.Screen
+          name="EmailRegister"
+          component={EmailRegisterScreen}
+          options={{ headerShown: false }}
+        />
+        {/* <Stack.Screen
           name="Player"
           component={PlayerModal}
           options={{ presentation: 'modal' }}
--- SPO.Client/app/types.ts
+++ SPO.Client/app/types.ts
@@ -1,5 +1,12 @@
 export type RootStackParamList = {
   Home: undefined; // Tab Navigator (Home, Search, Library)
-  Album: { id: string }; // Album screen with an 'id' parameter
-  PlayerModal: undefined; // Player modal
+  // Album: { id: string }; // Album screen with an 'id' parameter
+  // PlayerModal: undefined; // Player modal
+  Register: undefined; // Register screen
+  EmailRegister: undefined; // Email register screen
+  EmailLogin: undefined; // Email login screen
+  DetailPlaylist: { id: string }; // Playlist detail screen with an 'id' parameter
+  AlbumScreen: { id: string }; // Album screen with an 'id' parameter
+  login: undefined; // Login screen
+  Main: { screen?: string };
 };
