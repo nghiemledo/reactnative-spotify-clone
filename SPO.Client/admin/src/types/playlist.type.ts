3c973c55e0e4dd45047c251e628714f137b222c0
*gWRU5u1c7q <nghiemledo@gmail.com>
2025-05-12 08:07:55 +0700

    Feat(admin): add Playlist interface to define playlist structure

---
+++ SPO.Client/admin/src/types/playlist.type.ts
@@ -0,0 +1,10 @@
+export interface Playlist {
+    id?: string;
+    title: string;
+    description?: string;
+    coverImage?: string;
+    isPublic?: boolean;
+    userId?: string;
+    createdAt?: string;
+    updatedAt?: string;
+}
