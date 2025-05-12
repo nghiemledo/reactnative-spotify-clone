b633213ed2e3db45c1d2a99e42a56b9e33e4bb45
nxt <nxtnguyenxuantruongxt@gmail.com>
2025-05-12 10:17:14 +0700

    feat(app): update Babel configuration and dependencies for Tamagui integration

--- SPO.Client/app/babel.config.js
+++ SPO.Client/app/babel.config.js
@@ -2,6 +2,17 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["react-native-reanimated/plugin"],
+    plugins: [
+      "react-native-reanimated/plugin",
+      [
+        "@tamagui/babel-plugin",
+        {
+          components: ["tamagui"],
+          config: "./tamagui.config.ts",
+          logTimings: true,
+          disableExtraction: process.env.NODE_ENV === "development",
+        },
+      ],
+    ],
   };
 };
