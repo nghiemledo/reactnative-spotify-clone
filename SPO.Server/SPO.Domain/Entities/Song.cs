77e4da1816b08e071b7106ae5aa245e4c5908992
*gWRU5u1c7q <nghiemledo@gmail.com>
2025-05-12 08:10:28 +0700

    Feat(server): add Counter property to Song entity

--- SPO.Server/SPO.Domain/Entities/Song.cs
+++ SPO.Server/SPO.Domain/Entities/Song.cs
@@ -8,6 +8,7 @@ public class Song : AuditableEntity<string>
     public string CoverImage { get; set; } = string.Empty;
     public string? GenreId { get; set; }
     public int Duration { get; set; }
+    public int Counter { get; set; } = 0;
     public string AudioUrl { get; set; } = string.Empty;
     public string? ArtistId { get; set; }
     public string? AlbumId { get; set; }
