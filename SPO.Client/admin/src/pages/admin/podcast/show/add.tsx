6675da44456f7ff610dccf867aae882716cb7bfb
*gWRU5u1c7q <nghiemledo@gmail.com>
2025-05-13 22:39:05 +0700

    Feat(admin): add components for managing podcast shows (add, edit, view)

---
+++ SPO.Client/admin/src/pages/admin/podcast/show/add.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageAddPodcastShow: React.FC = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ManageAddPodcastShow;
---
+++ SPO.Client/admin/src/pages/admin/podcast/show/edit.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageEditPodcastShow: React.FC = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ManageEditPodcastShow;
