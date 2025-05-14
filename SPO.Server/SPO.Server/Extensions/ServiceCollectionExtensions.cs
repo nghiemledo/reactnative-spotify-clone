65d1ee98e7d6421e86fa2e96c447e9cd0bac65bc
nxt <nxtnguyenxuantruongxt@gmail.com>
2025-05-15 06:48:33 +0700

    Fix(cors): allow all origins in CORS policy for flexibility Fix(docker-compose): update connection string to use environment variable for security

--- SPO.Server/SPO.Server/Extensions/ServiceCollectionExtensions.cs
+++ SPO.Server/SPO.Server/Extensions/ServiceCollectionExtensions.cs
@@ -13,7 +13,7 @@ namespace SPO.Server.Extensions
                 options.AddPolicy(name: "spo-clone",
                                   policy =>
                                   {
-                                      policy.WithOrigins("http://localhost:3000", "https://spoc.vercel.app", "http://localhost:5173")
+                                      policy.WithOrigins("*")
                                       .AllowAnyHeader()
                                       .AllowAnyMethod()
                                       .AllowCredentials();
