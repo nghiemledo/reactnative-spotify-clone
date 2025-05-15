dc7c8f7f305b2d08a7d24c33d4dedef9a34a82a2
nxt <nxtnguyenxuantruongxt@gmail.com>
2025-05-15 16:42:48 +0700

    Feat(app): implement song services with endpoints for fetching songs and song details

---
+++ SPO.Client/app/services/SongService.ts
@@ -0,0 +1,40 @@
+import { baseRestApi } from "./api";
+import { ApiResponse } from "../types/apiResponse";
+import { Song } from "../types/song";
+
+const entity = "song";
+const SONG_TAG = "Song";
+
+export const songServices = baseRestApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getSongs: builder.query<{ data: Song[]; message: string }, void>({
+      query: () => ({
+        url: `${entity}`,
+        method: "GET",
+      }),
+      transformResponse: (response: ApiResponse<Song[]>) => ({
+        data: response.data,
+        message: response.message,
+      }),
+      providesTags: [{ type: SONG_TAG, id: "LIST" }],
+    }),
+    getSongById: builder.query<{ data: Song; message: string }, string>({
+      query: (id = "") => ({
+        url: `${entity}/${id}`,
+        method: "GET",
+      }),
+      transformResponse: (response: ApiResponse<Song>) => ({
+        data: response.data,
+        message: response.message,
+      }),
+      providesTags: (result, error, id) => [{ type: SONG_TAG, id }],
+    }),
+  }),
+});
+export const {
+  useGetSongsQuery,
+  useLazyGetSongsQuery,
+  useGetSongByIdQuery,
+  useLazyGetSongByIdQuery,
+  usePrefetch: songPrefetch,
+} = songServices;
