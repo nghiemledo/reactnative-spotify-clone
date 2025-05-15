8233c669c52828c33417d3367539cb50dcc1d995
DESKTOP-4ONK187\ACER NITRO 5 <hoanglong200703@gmail.com>
2025-05-15 15:09:58 +0700

    Feat(app): add updateSongPlaylist screen and update stack navigator

--- SPO.Client/app/navigation/TabNavigator.tsx
+++ SPO.Client/app/navigation/TabNavigator.tsx
@@ -1,14 +1,16 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
 import PlayingScreen from "../screens/PlayingScreen";
+import DetailPlaylistScreen from "../screens/DetailPlaylistScreen";
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Playing" component={PlayingScreen} />
+      {/* <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Playing" component={PlayingScreen} /> */}
+      <Tab.Screen name="Detail" component={DetailPlaylistScreen}  />
     </Tab.Navigator>
   );
 }
