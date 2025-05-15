121d01c974f8af171f9e03353a200ff33e5f614c
nxt <nxtnguyenxuantruongxt@gmail.com>
2025-05-15 17:48:19 +0700

    Feat(app): add SplashNavigator for improved app onboarding flow

---
+++ SPO.Client/app/navigation/SplashNavigator.tsx
@@ -0,0 +1,33 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import TabNavigator from "./TabNavigator";
+import LoginScreen from "../screens/LoginScreen";
+import SplashScreen from "../screens/SplashScreen";
+
+const Stack = createStackNavigator();
+export type SlpashStackParamList = {
+  Splash: undefined;
+  Main: undefined;
+  Login: undefined;
+};
+export default function SplashNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Splash"
+        component={SplashScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Main"
+        component={TabNavigator}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}
