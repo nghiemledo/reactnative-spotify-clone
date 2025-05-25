944fbcfa490d9c9483fa1199593b5d3135a6841b
Nguyễn Xuân Trường <nxtnguyenxuantruongxt@gmail.com>
2025-05-25 21:59:01 +0700

    feat(app): simplify QueueBottomSheet by removing unused props and integrating SongItem component

--- SPO.Client/app/components/QueueBottomSheet.tsx
+++ SPO.Client/app/components/QueueBottomSheet.tsx
@@ -1,77 +1,44 @@
-import React, { useCallback, useEffect, useState, useRef } from "react";
-import { XStack, Text, Button, Image, YStack } from "tamagui";
+import React, { useCallback } from "react";
+import { Text, YStack } from "tamagui";
 import BottomSheet, {
   BottomSheetBackdrop,
   BottomSheetView,
 } from "@gorhom/bottom-sheet";
-import { Dimensions, TouchableOpacity } from "react-native";
-import {
-  Play,
-  Pause,
-  Menu,
-  Shuffle,
-  Repeat,
-  Timer,
-  Repeat1,
-} from "@tamagui/lucide-icons";
-import DraggableFlatList, {
-  ScaleDecorator,
-} from "react-native-draggable-flatlist";
-// import TimerBottomSheet from "./TimerBottomSheet";
-
-interface QueueItem {
-  id: string;
-  title: string;
-  description: string;
-  image: string;
-}
+import { Dimensions } from "react-native";
+import { useAppSelector } from "../store";
+import { SongItem } from "./SongItem";
+import { Track } from "react-native-track-player";
 
 interface QueueBottomSheetProps {
   isOpen: boolean;
   onClose: () => void;
-  playlistName: string;
-  queueItems: QueueItem[];
-  onDone: () => void;
-  currentSongId: string | null;
-  isPlaying: boolean;
-  onTogglePlay: (songId: string) => void;
-  onReorder: (newQueueItems: QueueItem[]) => void;
 }
 
 const QueueBottomSheet: React.FC<QueueBottomSheetProps> = ({
   isOpen,
   onClose,
-  playlistName,
-  queueItems,
-  onDone,
-  currentSongId,
-  isPlaying,
-  onTogglePlay,
-  onReorder,
 }) => {
   const bottomSheetRef = React.useRef<BottomSheet>(null);
   const screenHeight = Dimensions.get("window").height;
   const snapPoints = ["50%", "95%"];
-  const [isShuffleEnabled, setIsShuffleEnabled] = useState(false);
-  const originalQueueRef = useRef<QueueItem[]>([]);
-  const [repeatMode, setRepeatMode] = useState(0); // 0: off, 1: repeat all, 2: repeat one
-  const [isTimerSheetOpen, setIsTimerSheetOpen] = useState(false);
-
-  const handleRepeatPress = () => {
-    setRepeatMode((prev) => (prev + 1) % 3);
-  };
-
-  // Store the original queue when the bottom sheet opens
-  useEffect(() => {
-    if (isOpen) {
-      originalQueueRef.current = [...queueItems];
-      bottomSheetRef.current?.expand();
-    } else {
-      bottomSheetRef.current?.close();
-      setIsShuffleEnabled(false);
-      setIsTimerSheetOpen(false); // Close TimerBottomSheet when QueueBottomSheet closes
-    }
-  }, [isOpen, queueItems]);
+  const { queue } = useAppSelector((state) => state.player);
+  const newQueue = queue.map((item: Track) => ({
+    id: item.id,
+    title: item.title,
+    coverImage:
+      typeof item.artwork === "string"
+        ? item.artwork
+        : "https://via.placeholder.com/150",
+    duration: item.duration,
+    audioUrl: typeof item.url === "string" ? item.url : String(item.url),
+    artist: item.artist,
+    genreId: undefined,
+    counter: undefined,
+    artistId: undefined,
+    albumId: undefined,
+    createdAt: undefined,
+    updatedAt: undefined,
+  }));
 
   const renderBackdrop = useCallback(
     (props: any) => (
@@ -96,43 +63,6 @@ const QueueBottomSheet: React.FC<QueueBottomSheetProps> = ({
     },
     [onClose]
   );
-
-  function fisherYatesShuffle(array: QueueItem[]) {
-    const shuffled = [...array];
-    for (let i = shuffled.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-    }
-    return shuffled;
-  }
-
-  const handleShuffle = () => {
-    const wasEnabled = isShuffleEnabled;
-    const newShuffleState = !wasEnabled;
-    setIsShuffleEnabled(newShuffleState);
-
-    if (newShuffleState) {
-      const currentSong = queueItems.find((item) => item.id === currentSongId);
-      const otherSongs = queueItems.filter((item) => item.id !== currentSongId);
-
-      const shuffledOthers = fisherYatesShuffle(otherSongs);
-
-      const newQueue = currentSong
-        ? [currentSong, ...shuffledOthers]
-        : shuffledOthers;
-
-      onReorder(newQueue);
-    } else {
-      onReorder([...originalQueueRef.current]);
-    }
-  };
-
-  // Handle timer selection
-  const handleSelectTimer = (duration: number) => {
-    console.log(`Timer set for ${duration} minutes`);
-    onDone(); // Call onDone after selecting a timer
-    setIsTimerSheetOpen(false); // Close TimerBottomSheet after selecting a timer
-  };
 
   return (
     <>
@@ -164,183 +94,20 @@ const QueueBottomSheet: React.FC<QueueBottomSheetProps> = ({
             >
               Queue
             </Text>
-            <XStack justify="flex-start" items="center" mb="$4">
-              <Text fontWeight={"300"} fontSize={16} color="white">
-                Now playing:
-              </Text>
-              <Text fontSize={16} color="white">
-                {playlistName || "My fourth playlist"}
-              </Text>
-            </XStack>
-
-            <DraggableFlatList
-              containerStyle={{ flex: 1 }}
-              data={queueItems}
-              keyExtractor={(item) => item.id}
-              renderItem={({ item, drag, isActive }) => {
-                const isCurrentSong = item.id === currentSongId;
-
-                return (
-                  <ScaleDecorator>
-                    <TouchableOpacity
-                      onPress={() => {
-                        onTogglePlay(item.id);
-                      }}
-                      disabled={isActive}
-                    >
-                      <XStack items="center" justify="space-between" py="$3">
-                        <XStack items="center" gap="$3" flex={1}>
-                          <Image
-                            source={{ uri: item.image }}
-                            width={50}
-                            height={50}
-                            borderRadius={8}
-                            defaultSource={{
-                              uri: "https://via.placeholder.com/50",
-                            }}
-                            onError={(e) =>
-                              console.log("Image load error:", e.nativeEvent)
-                            }
-                          />
-                          <YStack flex={1}>
-                            <Text
-                              fontSize={15}
-                              fontWeight="300"
-                              color="white"
-                              numberOfLines={1}
-                              ellipsizeMode="tail"
-                              pr={"$3"}
-                            >
-                              {item.title}
-                            </Text>
-                            <Text fontSize={13} color="white" opacity={0.7}>
-                              {item.description}
-                            </Text>
-                          </YStack>
-                        </XStack>
-                        {isCurrentSong ? (
-                          <Button
-                            m={0}
-                            p={0}
-                            rounded={100}
-                            width="$2"
-                            height="$2"
-                            icon={
-                              isPlaying ? (
-                                <Pause
-                                  color="black"
-                                  fill="black"
-                                  strokeWidth={1}
-                                />
-                              ) : (
-                                <Play
-                                  color="black"
-                                  fill="black"
-                                  strokeWidth={1}
-                                />
-                              )
-                            }
-                            onPress={() => {
-                              onTogglePlay(item.id);
-                            }}
-                          />
-                        ) : (
-                          <Button
-                            m={0}
-                            p={0}
-                            width="$2"
-                            height="$2"
-                            bg="transparent"
-                            icon={
-                              <Menu color="white" size="$1" strokeWidth={2} />
-                            }
-                            onLongPress={drag}
-                            onPress={() =>
-                              console.log("Menu for song:", item.title)
-                            }
-                          />
-                        )}
-                      </XStack>
-                    </TouchableOpacity>
-                  </ScaleDecorator>
-                );
-              }}
-              onDragEnd={({ data }) => {
-                onReorder(data);
-              }}
-              scrollEnabled={true}
-            />
-
-            <XStack justify="space-between" mt="$4" gap={"$2"}>
-              <Button
-                flex={1}
-                size="$5"
-                bg="gray"
-                color="white"
-                onPress={handleShuffle}
-              >
-                <YStack justify="center" items="center">
-                  <Shuffle color={isShuffleEnabled ? "#1DB954" : "white"} />
-                  <Text
-                    fontSize={12}
-                    color={isShuffleEnabled ? "#1DB954" : "white"}
-                    text="center"
-                  >
-                    Shuffle
-                  </Text>
-                </YStack>
-              </Button>
-              <Button flex={1} size="$5" bg="gray" onPress={handleRepeatPress}>
-                <YStack justify="center" items="center">
-                  {repeatMode === 2 ? (
-                    <Repeat1 color="green" />
-                  ) : (
-                    <Repeat color={repeatMode === 1 ? "green" : "white"} />
-                  )}
-                  <Text
-                    fontSize={12}
-                    color={repeatMode === 0 ? "white" : "green"}
-                    text="center"
-                  >
-                    Repeat
-                  </Text>
-                </YStack>
-              </Button>
-              <Button
-                flex={1}
-                size="$5"
-                bg="gray"
-                color="white"
-                onPress={() => {
-                  console.log(
-                    "Timer button pressed, setting isTimerSheetOpen to true"
-                  );
-                  setIsTimerSheetOpen(true); // Always open TimerBottomSheet
+            {newQueue.map((item, index) => (
+              <SongItem
+                key={item.id}
+                song={item}
+                showImage={true}
+                onMorePress={() => {
+                  console.log("More options pressed for song:", item.title);
+                  // Handle more options logic here
                 }}
-              >
-                <YStack justify="center" items="center">
-                  <Timer color={"white"} />
-                  <Text fontSize={12} color="white" text="center">
-                    Timer
-                  </Text>
-                </YStack>
-              </Button>
-            </XStack>
+              />
+            ))}
           </YStack>
         </BottomSheetView>
       </BottomSheet>
-
-      {/* Render TimerBottomSheet */}
-      {/* <TimerBottomSheet
-        isOpen={isTimerSheetOpen}
-        onClose={() => {
-          console.log(
-            "TimerBottomSheet onClose called, setting isTimerSheetOpen to false"
-          );
-          setIsTimerSheetOpen(false);
-        }}
-        onSelectTimer={handleSelectTimer}
-      /> */}
     </>
   );
 };
