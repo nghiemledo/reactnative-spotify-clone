18dbbd80e5200934e479ad0c111ff8d81bfb41bf
nxt <nxtnguyenxuantruongxt@gmail.com>
2025-05-25 15:36:23 +0700

    feat(app): initialize Google Mobile Ads SDK in App component

--- SPO.Client/app/App.tsx
+++ SPO.Client/app/App.tsx
@@ -1,17 +1,29 @@
-import { TamaguiProvider } from "@tamagui/core";
 import AppNavigator from "./navigation/AppNavigator";
+import { TamaguiProvider } from "@tamagui/core";
 import { config } from "./tamagui.config";
 import { Provider } from "react-redux";
 import { persistor, store } from "./store/index";
 import { PersistGate } from "redux-persist/integration/react";
 import { useEffect } from "react";
-import { playbackService, setupPlayer } from "./services/playerService";
+import { setupPlayer } from "./services/playerService";
 import { PortalProvider } from "tamagui";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { MobileAds } from "react-native-google-mobile-ads";
 
 const App = () => {
   useEffect(() => {
+    // Khởi tạo Google Mobile Ads SDK
+    MobileAds()
+      .initialize()
+      .then(() => {
+        console.log("✅ Khởi tạo Mobile Ads SDK thành công");
+      })
+      .catch((error: Error) => {
+        console.error("❌ Lỗi khởi tạo Mobile Ads SDK:", error);
+      });
+
+    // Khởi tạo player
     setupPlayer();
     // playbackService();
   }, []);
