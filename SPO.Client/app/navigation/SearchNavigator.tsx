0d4c92124dbf0c04ab154ed614bb3c5742eb87d8
yoursi <tranguyenvannhut@gmail.com>
2025-05-28 00:13:13 +0700

    feat(app): add GenreScreen to SearchNavigator and update navigation parameters

--- SPO.Client/app/navigation/SearchNavigator.tsx
+++ SPO.Client/app/navigation/SearchNavigator.tsx
@@ -2,6 +2,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import SearchScreen from "../screens/search/SearchScreen";
 import AddToPlaylistScreen from "../screens/playlists/AddToPlaylistScreen";
 import ScanScreen from "../screens/ScanScreen";
+import GenreScreen from "../screens/GenreScreen";
 
 const Stack = createStackNavigator();
 
@@ -9,6 +10,7 @@ export type SearchStackParamList = {
   SearchScreen: undefined;  
   AddToPlaylist: undefined;
   ScanScreen: undefined;
+  Genre: { id: string };
 };
 
 export default function SearchNavigator() {
@@ -27,6 +29,11 @@ export default function SearchNavigator() {
       <Stack.Screen
         name="ScanScreen"
         component={ScanScreen} // Giả sử bạn có một
+        options={{ headerShown: false }} // Thay thế bằng
+      />
+      <Stack.Screen
+        name="Genre"
+        component={GenreScreen} 
         options={{ headerShown: false }} // Thay thế bằng
       />
     </Stack.Navigator>
