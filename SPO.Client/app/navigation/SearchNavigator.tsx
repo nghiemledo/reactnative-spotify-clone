96f96bd8ef8fa4851166420912fefa22be1be55e
DESKTOP-4ONK187\ACER NITRO 5 <hoanglong200703@gmail.com>
2025-05-29 18:30:22 +0700

    feat(app): update SearchNavigator to include SearchResultScreen and clean up AppNavigator

--- SPO.Client/app/navigation/SearchNavigator.tsx
+++ SPO.Client/app/navigation/SearchNavigator.tsx
@@ -3,14 +3,16 @@ import SearchScreen from "../screens/search/SearchScreen";
 import AddToPlaylistScreen from "../screens/playlists/AddToPlaylistScreen";
 import ScanScreen from "../screens/ScanScreen";
 import GenreScreen from "../screens/GenreScreen";
+import SearchResultScreen from "../screens/search/SearchResultScreen";
 
 const Stack = createStackNavigator();
 
 export type SearchStackParamList = {
-  SearchScreen: undefined;  
+  SearchScreen: undefined;
   AddToPlaylist: undefined;
   ScanScreen: undefined;
   Genre: { id: string };
+  SearchResult: { toastMessages?: string[] };
 };
 
 export default function SearchNavigator() {
@@ -33,8 +35,13 @@ export default function SearchNavigator() {
       />
       <Stack.Screen
         name="Genre"
-        component={GenreScreen} 
+        component={GenreScreen}
         options={{ headerShown: false }} // Thay thế bằng
+      />
+      <Stack.Screen
+        name="SearchResult"
+        component={SearchResultScreen}
+        options={{ headerShown: false }}
       />
     </Stack.Navigator>
   );
